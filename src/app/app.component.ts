664d73699b2c4cb20592c489418985fc968f9191
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-06-10 16:46:31 +0300

    place elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from './shared/services/currency.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'currency-converter';
+
+  constructor(private currencyService: CurrencyService){}
+
+  ngOnInit() {
+    this.currencyService.getAllGames().subscribe(res => {
+      console.log(res.rates)
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ConverterComponent } from './components/converter/converter.component';
+import { InputComponent } from './shared/components/input/input.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConverterComponent,
+    InputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
