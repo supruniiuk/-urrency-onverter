9465c1c35a72c5aed62e529970ae4a850a96fc1b
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-06-10 23:04:27 +0300

    updated amount  input

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AmountForm } from 'src/app/components/converter/converter.component';
 
 @Component({
   selector: 'app-input',
@@ -6,7 +15,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.scss'],
 })
 export class InputComponent implements OnInit {
+  amount: FormGroup;
+
+  @Input() value: AmountForm;
+  @Output() currency: EventEmitter<any> = new EventEmitter();
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.amount = new FormGroup({
+      amount: new FormControl(this.value.amount),
+      currency: new FormControl(this.value.currency),
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.amount) {
+      this.setFormValue();
+    }
+  }
+
+  setFormValue() {
+    if (Number.isNaN(this.value.amount)) {
+      this.amount.controls['amount'].setValue(0);
+    } else {
+      this.amount.controls['amount'].setValue(this.value.amount);
+    }
+    this.amount.controls['currency'].setValue(this.value.currency);
+  }
+
+  convert(): void {
+    this.currency.emit(this.amount.value);
+  }
 }
