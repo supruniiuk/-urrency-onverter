ce27cd336b2ea052b706a91b0f2b4692b19eadd0
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-06-10 23:05:03 +0300

    updated header view

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Rate } from 'src/app/shared/interfaces/rate.interface';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Input() EUR: Rate | null;
+  @Input() USD: Rate | null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
